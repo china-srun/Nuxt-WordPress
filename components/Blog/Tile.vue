ff1137e7af4ab178a1bcd3ec3c32cb0513451ef5
SrunChina <china.srunn@gmail.com>
2023-02-27 00:06:50 +0700

    add tile view

---
+++ components/Blog/Tile.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="flex flex-col justify-center">
+    <div
+      class=" mb-5 relative flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-5xl mx-auto border border-white bg-white col-span-1 transform transition duration-500 hover:scale-105"
+    >
+      <div class="w-full md:w-1/3 bg-white grid place-items-center">
+        <img
+          :src="image"
+          alt="Thumbnail"
+          class="w-50 h-60 object-cover rounded p-3"
+        />
+      </div>
+      <div class="w-full md:w-2/3 bg-white flex flex-col space-y-2 p-3">
+        <h3 class="font-black text-gray-800 md:text-3xl text-xl">
+          <div v-html="title"></div>
+        </h3>
+        <div class="md:text-lg text-gray-500 text-base">
+          <div v-html="description"></div>
+        </div>
+        <div class="mt-6 mb-2 flex justify-end">
+          <NuxtLink :to="`/home/${slug}`" class="bg-gray-200 p-2 rounded align-middle">Read More</NuxtLink>
+        </div >
+      </div>
+    </div>
+  </div>
+</template>
+  
+  <script setup lang="ts">
+defineProps<{
+  title: string;
+  description?: string;
+  image: string;
+  slug: string;
+}>();
+</script>
+  
+  <style scoped>
+</style>
--- pages/home/index.vue
+++ pages/home/index.vue
@@ -13,10 +13,25 @@
       </div>
     </PageHeader>
     <!-- Block Grid -->
-    <section class="container py-16">
+    <section class="container py-9">
+      <div class="mb-9 flex justify-end">
+        <a class="btn bg-gray-500 text-sky-50 py-2 px-2 rounded inline-block hover:bg-gray-600 duration-200" @click="onClickGrid">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M9 4.5v15m6-15v15m-10.875 0h15.75c.621 0 1.125-.504 1.125-1.125V5.625c0-.621-.504-1.125-1.125-1.125H4.125C3.504 4.5 3 5.004 3 5.625v12.75c0 .621.504 1.125 1.125 1.125z" />
+          </svg>
+        </a>
+        <a class="btn bg-gray-500 text-sky-50 py-2 px-2 rounded inline-block hover:bg-gray-600 duration-200 ml-3" @click="onClickTile">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+          </svg>
+        </a>
+      </div>
       <!-- show 3 cols when the screen minimum size is small -->
-      <div class="grid sm:grid-cols-3 gap-5">
+      <div class="grid sm:grid-cols-3 gap-5" v-show="grid">
         <BlogCard v-for="post in posts.posts" :key="post.ID" :title="post.title" :image="post.author.avatar_URL" :slug="post.slug" :description="post.excerpt"></BlogCard>
+      </div>
+      <div v-show="tile">
+        <BlogTile v-for="post in posts.posts" :key="post.ID" :title="post.title" :image="post.author.avatar_URL" :slug="post.slug" :description="post.excerpt"></BlogTile>
       </div>
     </section>
 </template>
@@ -25,7 +40,18 @@
 // const {data:posts} = useFetch<any>("https://public-api.wordpress.com/rest/v1.1/sites/kirirominstituteoftechnology.wordpress.com/posts?number=100");
 // console.log(posts);
 const {data:posts} = await useWpApi().getPosts<any>();
+const grid = ref(true)
+const tile = ref(false)
 
+function onClickGrid() {
+  grid.value = true;
+  tile.value = false;
+
+}
+function onClickTile() {
+  tile.value = true;
+  grid.value = false;
+}
 </script>
 
 <style scoped>
